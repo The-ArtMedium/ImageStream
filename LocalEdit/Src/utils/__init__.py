62f25d1e97844cae7ce9d9bb59a5564fbd68f39d
Jeena Sant Ji <jeenajah@gmail.com>
2026-02-17 08:59:09 -0500

    Update __init__.py

--- LocalEdit/Src/utils/__init__.py
+++ LocalEdit/Src/utils/__init__.py
@@ -4,5 +4,14 @@ Helper functions and utilities.
 """
 
 from .file_handler import FileHandler
+from .locale_manager import LocaleManager, get_locale_manager, translate, t
+from .config import get_config
 
-__all__ = ['FileHandler']
+__all__ = [
+    'FileHandler',
+    'LocaleManager',
+    'get_locale_manager',
+    'translate',
+    't',
+    'get_config'
+]
