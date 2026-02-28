4b4cc99ba55f68b03afb99d2851f0542de91445b
Jeena Sant Ji <jeenajah@gmail.com>
2026-02-28 10:03:30 -0500

    Update preview_widget.py

--- LoacalRaw/app/ui/preview_widget.py
+++ LoacalRaw/app/ui/preview_widget.py
@@ -1,1 +1,11 @@
---
+from PySide6.QtWidgets import QLabel
+from PySide6.QtCore import Qt
+
+
+class PreviewWidget(QLabel):
+    def __init__(self):
+        super().__init__()
+
+        self.setAlignment(Qt.AlignCenter)
+        self.setText("Preview Area")
+        self.setStyleSheet("background-color: #222; color: white;")--
