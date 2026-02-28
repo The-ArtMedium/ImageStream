498a470d36443740b2b3b3a3201192b27a51197e
Jeena Sant Ji <jeenajah@gmail.com>
2026-02-28 10:04:25 -0500

    Update control_panel.py

--- LoacalRaw/app/ui/control_panel.py
+++ LoacalRaw/app/ui/control_panel.py
@@ -1,1 +1,16 @@
---
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtCore import Qt
+
+
+class ControlsPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        title = QLabel("Adjustments")
+        title.setAlignment(Qt.AlignCenter)
+
+        layout.addWidget(title)
+        layout.addStretch()--
